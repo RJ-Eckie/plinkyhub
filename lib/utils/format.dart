52ef7d2d63b9e0d4bdcd946670b11bbe91c2f700
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2026-03-22 21:54:34 +0100

    Fix _round function displaying values 10x too large
    The function multiplied by 100 (10*10) but only divided by 10, causing
    negative inputs like -1 to display as -9.8 instead of -1.0.

--- lib/utils/format.dart
+++ lib/utils/format.dart
@@ -2,7 +2,6 @@ double normalize(int x) => x / 1024 * 100;
 
 double denormalize(double x) => x / 100 * 1024;
 
-double _round(double num) =>
-    ((num * 10 + double.minPositive) * 10).roundToDouble() / 10;
+double _round(double num) => (num * 10).roundToDouble() / 10;
 
 String formatValue(int v) => _round(normalize(v)).toStringAsFixed(1);
